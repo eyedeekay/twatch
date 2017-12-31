d9db93c2bbd77620ea7dd3cf653eb451c8024be5
idk <problemsolver@openmailbox.org>
2017-12-31 04:22:38 -0500

    monitor mode functions

--- tshark/tshark-functions.sh
+++ tshark/tshark-functions.sh
@@ -1,16 +1,32 @@
 #! /usr/bin/env bash
 
 if [ -f tshark/tshark.conf ]; then
-    source tshark.conf
+    source tshark/tshark.conf
 fi
 
 monitor(){
-    sudo ifconfig "$wifi_interface" down
-    sudo iwconfig "$wifi_interface" mode monitor
-    sudo iwconfig "$wifi_interface" channel 6
-    sudo ifconfig "$wifi_interface" up
+    sudo ifconfig "$wifi_interface" down && \
+        echo "Brought $wifi_interface down"
+    sudo iwconfig "$wifi_interface" mode monitor && \
+        echo "Put $wifi_interface into monitor mode"
+    sudo ifconfig "$wifi_interface" up && \
+        echo "Brought $wifi_interface back up"
+    sudo iwconfig "$wifi_interface" channel 6 && \
+        echo "Setting $wifi_interface to channel 6"
+}
+
+client(){
+    sudo ifconfig "$wifi_interface" down && \
+        echo "Brought $wifi_interface down"
+    sudo iwconfig "$wifi_interface" mode managed && \
+        echo "Put $wifi_interface into managed client mode"
+    sudo ifconfig "$wifi_interface" up && \
+        echo "Brought $wifi_interface back up"
+    sudo iwconfig "$wifi_interface" channel 6 && \
+        echo "Set $wifi_interface to channel 6"
 }
 
 beacons(){
-    tshark -i "$wifi_interface"
+    tshark -n -i "$wifi_interface" subtype probereq
 }
+
